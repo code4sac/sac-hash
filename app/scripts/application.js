4a4b259f2acd63018b64a27028f83ad65d35c77c
Andy Axton <dizzysfarm@gmail.com>
2013-09-26 17:35:06 -0700

    interface changes, added bootstrap

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -13,39 +13,28 @@ function( Backbone, Communicator, map, tweetParse, nbhoodsCollection, tweet_tmp)
 	var App = new Backbone.Marionette.Application();
 
 	/* Add application regions here */
-	App.addRegions({});
+	App.addRegions({
+
+	});
 
 	/* Add initializers here */
 	App.addInitializer( function () {
 		
 	});
 	var count = 0;
-	console.log(nbhoodsCollection)
+
 	var socket = io.connect('http://localhost:9000');
 	  
 	  socket.on('tweet', function (data) {
 	    var tweet = linkify_entities(data);
 	    data.entities.text = tweet
-
+	    
 	    count++;
 
-	    $('.tweet-container').append(tweet_tmp(data));
+	    $('.tweet-container .tweets').append(tweet_tmp(data));
 	    $('#count span').text(count);
-
-	    socket.emit('my other event', { my: 'data' });
-	  });
-    	
-	// console.log(nbhoods)
-	// for (var i = 0; i <= 129 ; i++){
-	// 	document.write(nbhoods['features'][i]['properties']['NAME']+'<br>')
-	// }
-	$.ajax({
-  		type: "GET",
-  		url: "/tweets/cfabrigade/200",
-	}).done(function(data) {
-		console.log(data)
-  		for (var i = 0; i < data['statuses'].length; i++){
-  			var geo = data['statuses'][i]['coordinates'],
+	    
+	    var geo = data['coordinates'],
   				lat,
   				lng,
   				point;
@@ -62,9 +51,40 @@ function( Backbone, Communicator, map, tweetParse, nbhoodsCollection, tweet_tmp)
         		});
         		
   			}
-  		}
+
+	    socket.emit('my other event', { my: 'data' });
+	  });
+    	
+	// console.log(nbhoods)
+	// for (var i = 0; i <= 129 ; i++){
+	// 	document.write(nbhoods['features'][i]['properties']['NAME']+'<br>')
+	// }
+	// $.ajax({
+ //  		type: "GET",
+ //  		url: "/tweets/sacramento/200",
+	// }).done(function(data) {
+		
+ //  		for (var i = 0; i < data['statuses'].length; i++){
+ //  			var geo = data['statuses'][i]['coordinates'],
+ //  				lat,
+ //  				lng,
+ //  				point;
+
+ //  			if (geo){
+ //  				lat = geo.coordinates[1];
+ //  				lng = geo.coordinates[0];
+
+ //  				point = new google.maps.LatLng(lat, lng);
+  				
+ //  				new google.maps.Marker({
+	// 	            map: map,
+	// 	            position: point,
+ //        		});
+        		
+ //  			}
+ //  		}
   		
-	});
+	// });
 
 	return App;
 });
