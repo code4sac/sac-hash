062253e27f48ff7e728a6379092a911928b1e2a3
Andy Axton <dizzysfarm@gmail.com>
2013-10-22 19:11:25 -0700

    ARGHHHH

--- scripts/collections/nbhoods-collection.js
+++ scripts/collections/nbhoods-collection.js
@@ -1,23 +1,41 @@
 define(['backbone','models/nbhood-model'], function( Backbone, nbhoodModel){
-	'use strict';
+  'use strict';
+  
+ var proxiedSync = Backbone.sync;
 
-	var nbhoodCollection = Backbone.Collection.extend({
-		model: nbhoodModel,
-		url: 'http://ec2-50-18-231-203.us-west-1.compute.amazonaws.com/server/twitterFeed/alldata.php',
-		// parse: function(response) {
-		// 	var result = [];
+  Backbone.sync = function(method, model, options) {
+    options || (options = {});
 
-		// 	// for (var i = 0; i < response.length; i++){
-		// 	// 	if (response[i].hashtag != ''){
-		// 	// 		result.push(response[i])
-		// 	// 	}
-		// 	// }
-		// 	console.log(true)
-  //  			return response;
-  //  			// return response;
-  // 		},
-  		parse: function(response) {
-        return response.results;
+    if (!options.crossDomain) {
+      options.crossDomain = true;
+    }
+
+    if (!options.xhrFields) {
+      options.xhrFields = {withCredentials:true};
+    }
+
+    return proxiedSync(method, model, options);
+  };
+
+  var nbhoodCollection = Backbone.Collection.extend({
+    model: nbhoodModel,
+    url: 'http://ec2-50-18-231-203.us-west-1.compute.amazonaws.com/data/alldata.php',
+    // parse: function(response) {
+    //  var result = [];
+
+    //  // for (var i = 0; i < response.length; i++){
+    //  //  if (response[i].hashtag != ''){
+    //  //    result.push(response[i])
+    //  //  }
+    //  // }
+    //  console.log(true)
+  //        return response;
+  //        // return response;
+  //    },
+ 
+    parse: function(response) {
+      console.log(response.responseText)
+      return response;
     },
 
     sync: function(method, model, options) {
@@ -26,22 +44,28 @@ define(['backbone','models/nbhood-model'], function( Backbone, nbhoodModel){
             type: 'GET',
             dataType: 'jsonp',
             url: that.url,
-            processData: false
+            processData: false,
         }, options);
-
+        
         return $.ajax(params);
     }
-	});
+  });
 
-	var Neighborhoods = new nbhoodCollection();
-	Neighborhoods.fetch({
-        success: function() {
-             console.log();
-        },
-        error: function() {
-             console.log('Failed to fetch!');
-        }
-   });
 
-	return Neighborhoods;
+
+  var Neighborhoods = new nbhoodCollection([]);
+    var onDataHandler = function(collection, response, options) {
+      // console.log('membersview fetch onedatahandler');
+      console.log('success', response)
+  };
+
+  var onErrorHandler = function(collection, response, options) {
+      // console.log('membersview fetch onerrorhandler');
+     
+      console.log('error', response);
+  };
+
+  Neighborhoods.fetch({ success : onDataHandler, error: onErrorHandler, dataType: 'jsonp' });
+
+  return Neighborhoods;
 })
