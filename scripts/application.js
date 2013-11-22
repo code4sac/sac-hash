bd14f40bc3dff80ad1857d7f7a77eb84874fcc3c
Andy Axton <dizzysfarm@gmail.com>
2013-11-21 18:35:02 -0800

    created backbone structure for tweets

--- scripts/application.js
+++ scripts/application.js
@@ -8,20 +8,33 @@ define([
 	'views/ranges-view',
 	'collections/ranges-collection',
 	'views/map-controls-view',
+	'collections/tweets-collection',
+	'views/tweets-view',
 	'hbs!tmpl/tweet',
 	'interface'
-],
-
-function( Backbone, Communicator, tweetParse, map, nbhoodsView, nbhoodsCollection, rangesView, rangesCollection, mapControlsView, tweet_tmp) {
+], function( 
+	Backbone,
+	Communicator,
+	tweetParse,
+	map,
+	nbhoodsView,
+	nbhoodsCollection,
+	rangesView,
+	rangesCollection,
+	mapControlsView,
+	tweetsCollection,
+	tweetsView,
+	tweet_tmp) {
     'use strict';
 
 	var App = new Backbone.Marionette.Application();
 
 	// application regions
 	App.addRegions({
-		blockView: '#block-view',
+		nbhoods: '#nbhoods',
 		mapKey: '#map-key',
-		mapControls: '#map-controls'
+		mapControls: '#map-controls',
+		tweets: '#tweet-feed'
 	});
 
 	// fetch neighborhood data
@@ -34,9 +47,10 @@ function( Backbone, Communicator, tweetParse, map, nbhoodsView, nbhoodsCollectio
     // render app after data is loaded
     nbhoodsCollection.on('sync', function(model, xhr, options){
    	  	App.addInitializer( function(){
-			App.blockView.show( new nbhoodsView({ collection: nbhoodsCollection }) );
+			App.nbhoods.show( new nbhoodsView({ collection: nbhoodsCollection }) );
 			App.mapKey.show( new rangesView({ collection: rangesCollection }) );
 			App.mapControls.show( new mapControlsView() );
+			App.tweets.show( new tweetsView({ collection: tweetsCollection }) );
 		});
 
 		nbhoodsCollection.off('sync');
