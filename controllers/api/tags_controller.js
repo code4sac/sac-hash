f3186965a7b019e91fc8950e9d0a8a746dd915a9
Derek Lindahl <dlindahl@customink.com>
2014-02-22 23:49:26 -0800

    Refactor tags controller to query MongoDB

--- controllers/api/tags_controller.js
+++ controllers/api/tags_controller.js
@@ -4,59 +4,21 @@
  * Module dependencies
  */
 
-var util = require('util'),
-    when = require('when'),
-    nodefn = require('when/node/function'),
-    extend = require('node.extend'),
-    mysql = require('mysql');
-
-/**
- * Query constants
- */
-
-var COUNT_QUERY = 'select count(tag) as count from tweet_tags where tag in (?)';
+var db = require('../../lib/storage');
 
 /**
  * ETL functions
  */
 
-function normalizeRows(data, row) {
-  return row.reduce(function(tag) {
-    data.columns.forEach(function(name, i) {
-      var value = row[i];
-      // Merge object literal values as is
-      if('object' === typeof value && !util.isArray(value)) {
-        extend(tag, value);
-      } else {
-        tag[name] = row[i];
-      }
+function mergeWithGeoProps(geoProps, counts) {
+  var propsWithCounts = geoProps.map(function(prop) {
+    prop.keywords.forEach(function(keyword) {
+      prop.count = counts[keyword.substr(1).toLowerCase()];
     });
-    return tag;
-  }, {});
-}
+    return prop;
+  });
 
-function geoMergedWithTweetCounts(pool, geoProps) {
-  return nodefn.call(pool.getConnection.bind(pool))
-    .then(mapPropsToTweetCounts.bind(null, geoProps));
-}
-
-function mapPropsToTweetCounts(geoProps, conn) {
-  return when.map(geoProps, countKeywordedTweets.bind(null, conn))
-          .finally(conn.release.bind(conn));
-}
-
-function countKeywordedTweets(conn, prop) {
-  var sql = mysql.format(COUNT_QUERY, [prop.keywords]);
-
-  return nodefn.call(conn.query.bind(conn), sql)
-          .then(addCounts.bind(null, prop))
-}
-
-function addCounts(prop, results) {
-  var rows = results[0],
-      count = rows[0].count;
-
-  return extend(prop, {count:count});
+  return propsWithCounts;
 }
 
 /**
@@ -76,10 +38,12 @@ function allErrors(res, err) {
  */
 
 function index(req, res) {
-  var geoProps = req.app.get('geoProps');
+  var geoProps = req.app.get('geoProps'),
+      keywords = req.app.get('keywords');
 
-  when(geoMergedWithTweetCounts(req.mysqlPool, geoProps))
-    .tap(renderTags.bind(null, res))
+  db.countByKeyword(keywords)
+    .then(mergeWithGeoProps.bind(null, geoProps))
+    .then(renderTags.bind(null, res))
     .catch(allErrors.bind(null, res));
 }
 
